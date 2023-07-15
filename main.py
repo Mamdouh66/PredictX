2ef41c14ccae460a7013ac76f9e317dee66861d0
Mamdouh66 <2210002016@iau.edu.sa>
2023-07-15 18:33:57 +0300

    Built the model

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import pandas_datareader as web
 import datetime as dt
-import tensorflow as tf
+import yfinance as yf
 
+from tensorflow.python.keras.layers import Dense, Dropout, LSTM
+from tensorflow.python.keras import Sequential
 from sklearn.preprocessing import MinMaxScaler  
+from pandas_datareader import data as pdr
+
+yf.pdr_override()
+crypto_currency = 'BTC'
+against_currency = 'USD'
+
+start = dt.datetime(2016, 1, 1)
+end = dt.datetime.now()
+
+data = pdr.get_data_yahoo(f'{crypto_currency}-{against_currency}', start, end)  
+
+# Prepare Data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+prediction_days = 60
+
+x_train, y_train = [], []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0]) # 60 days
+    y_train.append(scaled_data[x,0]) # the day after
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+# Build The Model
+
+model = Sequential()
