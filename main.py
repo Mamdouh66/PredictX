f7fe0cf765cac218b07e6836af8b8050c44de51d
Mamdouh66 <2210002016@iau.edu.sa>
2023-07-15 18:05:58 +0300

    init. project

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pandas_datareader as web
+import datetime as dt
+import tensorflow as tf
+
+from sklearn.preprocessing import MinMaxScaler  
